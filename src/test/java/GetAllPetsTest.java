50f74256425b256f2515819070fa6bdaeb86db68
Rekha2205 <rekhachowdary2205@gmail.com>
2026-03-25 18:47:59 +0530

    Initial Maven project setup with RestAssured and GetAllPets API test with Assertion

---
+++ src/test/java/GetAllPetsTest.java
@@ -0,0 +1,23 @@
+package basic;
+
+import io.restassured.RestAssured;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+public class GetAllPetsTest {
+
+    @Test
+    public void getAllPets() {
+
+        RestAssured.baseURI = "https://petstore.swagger.io";
+
+        given()
+                .when()
+                .get("/v2/pet/findByStatus?status=available")
+                .then()
+                .statusCode(200)
+                .log().all();
+    }
+}
