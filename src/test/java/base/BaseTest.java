b18b8116953c7f7e00b3f0fe68f604ef3cd2a169
Rekha2205 <rekhachowdary2205@gmail.com>
2026-04-07 15:47:15 +0530

    TC1: Add BaseTest for REST Assured base URI configuration

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -7,5 +7,8 @@ public class BaseTest {
     static {
         RestAssured.baseURI = "https://petstore.swagger.io/v2";
         RestAssured.useRelaxedHTTPSValidation();
+
+        // IMPORTANT: avoids connection reuse issues
+        System.setProperty("http.keepAlive", "false");
     }
 }
