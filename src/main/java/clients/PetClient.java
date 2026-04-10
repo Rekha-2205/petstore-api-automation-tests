618f3867f391f601d4f2f701aecf19f107d6a8e3
Rekha2205 <rekhachowdary2205@gmail.com>
2026-04-10 10:42:42 +0530

    TC1:Implement PetClient to handle CRUD operations for Pet API with reusable request methods

--- src/main/java/clients/PetClient.java
+++ src/main/java/clients/PetClient.java
@@ -1,66 +1,57 @@
 package clients;
 
+import io.restassured.RestAssured;
 import io.restassured.response.Response;
-
-import static io.restassured.RestAssured.given;
 
 public class PetClient {
 
-    private static final String BASE_URL = "https://petstore.swagger.io/v2";
+    static {
+        RestAssured.baseURI = "https://petstore.swagger.io/v2";
+    }
 
     // CREATE PET
     public Response createPet(long id, String name, String status) {
 
-        return given()
+        String body = "{\n" +
+                "  \"id\": " + id + ",\n" +
+                "  \"name\": \"" + name + "\",\n" +
+                "  \"status\": \"" + status + "\"\n" +
+                "}";
+
+        return RestAssured
+                .given()
                 .header("Content-Type", "application/json")
-                .body("{\n" +
-                        "  \"id\": " + id + ",\n" +
-                        "  \"name\": \"" + name + "\",\n" +
-                        "  \"status\": \"" + status + "\"\n" +
-                        "}")
-                .when()
-                .post(BASE_URL + "/pet")
-                .then()
-                .extract()
-                .response();
+                .body(body)
+                .post("/pet");
     }
 
     // GET PET
-    public Response getPet(long petId) {
-
-        return given()
-                .when()
-                .get(BASE_URL + "/pet/" + petId)
-                .then()
-                .extract()
-                .response();
+    public Response getPet(long id) {
+        return RestAssured
+                .given()
+                .get("/pet/" + id);
     }
 
     // UPDATE PET
     public Response updatePet(long id, String name, String status) {
 
-        return given()
+        String body = "{\n" +
+                "  \"id\": " + id + ",\n" +
+                "  \"name\": \"" + name + "\",\n" +
+                "  \"status\": \"" + status + "\"\n" +
+                "}";
+
+        return RestAssured
+                .given()
                 .header("Content-Type", "application/json")
-                .body("{\n" +
-                        "  \"id\": " + id + ",\n" +
-                        "  \"name\": \"" + name + "\",\n" +
-                        "  \"status\": \"" + status + "\"\n" +
-                        "}")
-                .when()
-                .put(BASE_URL + "/pet")
-                .then()
-                .extract()
-                .response();
+                .body(body)
+                .put("/pet");
     }
 
     // DELETE PET
-    public Response deletePet(long petId) {
-
-        return given()
-                .when()
-                .delete(BASE_URL + "/pet/" + petId)
-                .then()
-                .extract()
-                .response();
+    public Response deletePet(long id) {
+        return RestAssured
+                .given()
+                .delete("/pet/" + id);
     }
 }
