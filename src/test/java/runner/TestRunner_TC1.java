2700abebd2c616ea1b54839f3d50ed99b88d99b9
Rekha2205 <rekhachowdary2205@gmail.com>
2026-04-10 10:49:09 +0530

    TC1:Configure Cucumber test runner for executing scenarios

--- src/test/java/runner/TestRunner_TC1.java
+++ src/test/java/runner/TestRunner_TC1.java
@@ -1,5 +1,6 @@
 package runner;
 
+import base.BaseTest;
 import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
@@ -9,8 +10,8 @@ import io.cucumber.junit.CucumberOptions;
         features = "src/test/resources/features/petLifecycle_TC1.feature",
         glue = "steps",
         tags = "@TC1",
-        plugin = {"pretty"},
+        plugin = {"pretty", "html:target/cucumber-tc1.html"},
         monochrome = true
 )
-public class TestRunner_TC1 {
+public class TestRunner_TC1 extends BaseTest {
 }
