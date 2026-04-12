934cb04866c8199aaf98a447879ef2fe47c184cf
Rekha2205 <rekhachowdary2205@gmail.com>
2026-04-12 13:14:22 +0530

    TC2: Implement StoreClient for /store/inventory API with logging and reusable request setup

---
+++ src/main/java/clients/StoreClient.java
@@ -0,0 +1,19 @@
+package clients;
+
+import io.restassured.response.Response;
+
+import static io.restassured.RestAssured.given;
+
+public class StoreClient {
+
+    public Response getInventory() {
+        return given()
+                .log().all()
+                .when()
+                .get("/store/inventory")
+                .then()
+                .log().all()
+                .extract()
+                .response();
+    }
+}
