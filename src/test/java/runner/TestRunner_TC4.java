214fd584a54c015fb9ece0e46937400d67074325
Rekha2205 <rekhachowdary2205@gmail.com>
2026-04-16 12:53:56 +0530

    TC4:Implemented Cucumber test runner for executing feature

---
+++ src/test/java/runner/TestRunner_TC4.java
@@ -0,0 +1,17 @@
+package runner;
+
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features/pet_TC4.feature",
+        glue = {"steps"},
+        tags = "@TC4",
+        plugin = {"pretty", "html:target/cucumber-reports-tc4.html"},
+        monochrome = true
+)
+public class TestRunner_TC4
+{
+}
